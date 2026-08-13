40d21f513dadb50ecd7796060163370c8773ee25
JJ <johnsonjustin983@gmail.com>
2026-08-13 19:27:49 -0400

    updated number

--- src/lib/AboutSection.svelte
+++ src/lib/AboutSection.svelte
@@ -56,7 +56,7 @@
               <span class="lbl">day avg. turnaround</span>
             </div>
             <div>
-              <span class="num">50+</span>
+              <span class="num">150+</span>
               <span class="lbl">hours digitized</span>
             </div>
             <div>
